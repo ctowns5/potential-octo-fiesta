e9add2e76b296f66a71fc359ec98e7f705b9ca96
ctowns5 <128754073+ctowns5@users.noreply.github.com>
2023-07-20 23:58:10 -0600

    fixed navbar logo, webpack.config, other edits, page loads and seems ok but install button does nothing

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
+const {GenerateSW} = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
@@ -40,7 +41,7 @@ module.exports = () => {
         ]
       }),
       new MiniCssExtractPlugin(),
-      new WorkboxPlugin.GenerateSW()
+      new GenerateSW()
     ],
     module: {
       rules: [
